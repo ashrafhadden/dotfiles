b2914db2a491635a2de439d44348fe877ec804cb
Ashraf Hadden <ashrafhadden@gmail.com>
2019-02-28 03:01:58 -0500

    Initial Customization

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")";
+cd "$(dirname "${BASH_SOURCE}")"
 
-git pull origin master;
+git pull origin master
 
 function doIt() {
 	rsync --exclude ".git/" \
@@ -11,17 +11,35 @@ function doIt() {
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
-		-avh --no-perms . ~;
-	source ~/.bash_profile;
+		-avh --no-perms . ~
+	# Load the shell dotfiles, and then some:
+	# * ~/.path can be used to extend `$PATH`.
+	# * ~/.extra can be used for other settings you don’t want to commit.
+	for file in ~/.{path,exports,aliases,functions,extra}; do
+		[ -r "$file" ] && [ -f "$file" ] && source "$file"
+	done
+	unset file
+
+	# Enable tab completion for `g` by marking it as an alias for `git`
+	if type _git &>/dev/null && [ -f /usr/local/etc/bash_completion.d/git-completion.bash ]; then
+		complete -o default -o nospace -F _git g
+	fi
+
+	# Add tab completion for `defaults read|write NSGlobalDomain`
+	# You could just use `-g` instead, but I like being explicit
+	complete -W "NSGlobalDomain" defaults
+
+	# Add `killall` tab completion for common apps
+	complete -o "nospace" -W "Contacts Calendar Dock Finder Mail Safari iTunes SystemUIServer Terminal Twitter" killall
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
+	doIt
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+	echo ""
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
-unset doIt;
+		doIt
+	fi
+fi
+unset doIt
